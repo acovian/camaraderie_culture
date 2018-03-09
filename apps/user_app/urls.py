299034eee5b667faa249032c6f6648b66952ad66
Alex Covian <alejandrocovian3@gmail.com>
2018-03-09 13:58:30 -0800

    adde completed camaraderie culture project

--- apps/user_app/urls.py
+++ apps/user_app/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name = 'index'),
-	url(r'^login$', views.login, name = 'login'),
-	url(r'^register$', views.register, name = 'register'),
-	url(r'^logout$', views.logout, name = 'logout'),
-	url(r'^user(?P<id>\d+)$', views.user, name = 'user'),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^login$', views.login, name = 'login'),
+    url(r'^register$', views.register, name = 'register'),
+    url(r'^logout$', views.logout, name = 'logout'),
+    url(r'^user(?P<id>\d+)$', views.user, name = 'user'),
 ]
